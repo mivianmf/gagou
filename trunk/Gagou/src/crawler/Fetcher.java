9484e99153fc9518198dcccb6d44686fad8aba37
fabricio127@gmail.com <fabricio127@gmail.com@6b8f7fa3-c1a2-4994-f7d0-45643b9cd2d7>
2013-08-23 23:10:10 +0000

    Fetcher

--- trunk/Gagou/src/crawler/Fetcher.java
+++ trunk/Gagou/src/crawler/Fetcher.java
@@ -1,5 +1,11 @@
 package crawler;
 
+
+/** 1 - Verificar se a URL é válida
+ *  2 - Resolver DNS
+ *  3 - Resolver Robots.txt
+ *  4 - http request response
+ *  5 - armazenar o documento */
 public class Fetcher {
 
 }
