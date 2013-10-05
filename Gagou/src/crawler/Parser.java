0a366901cb34fdcd231c4e5cbb687e6adcb2605a
mivianmarques@gmail.com <mivianmarques@gmail.com@6b8f7fa3-c1a2-4994-f7d0-45643b9cd2d7>
2013-10-05 17:48:04 +0000

    TODO ok - link relativo

--- Gagou/src/crawler/Parser.java
+++ Gagou/src/crawler/Parser.java
@@ -24,7 +24,6 @@ public class Parser {
 		File in = new File("arquivos\\fetchedPages\\" + removeBarra(url) + ".html");
 		Document doc = Jsoup.parse(in, null); //abrir o arquivo e fazer o parse nele
 		
-		
 		Elements links = doc.select("a[href]"); //pegar todos os links
 		
 		for (Element link : links) { //para cada link da lista
@@ -33,16 +32,51 @@ public class Parser {
 			
 			//validar não precisa, crawler sempre valida antes de passar url pra um fetcher
 			
-			
 			if ( !novaURL.equals("") ) { //nova url não é vazia
-				
+			
+				if (novaURL.contains("..")){
+					novaURL = voltarPasta (url, novaURL);
+				}
 //				System.out.println("Adicionando nova url na lista: " + novaURL);
+				
 				Crawler.urls.add(novaURL);	
 				// TODO - Transformar URL (colocar protocolo quando nao tiver)
 			}
-        }
-		
+		}
 	}
+	
+	
+	private String voltarPasta(String url, String novaURL){
+		String resposta = "";
+		int contPontos = 0;
+		int html = 0;
+		
+		String [] relativo = novaURL.split("/");
+		String [] absoluto = url.split("/");
+		
+		for (int i = 0; i < relativo.length; i++) {
+			if(relativo[i].equals("..")){
+				contPontos++;	
+			}
+		}
+
+		if (absoluto[absoluto.length-1].contains(".html")){
+			html = 1;
+		}
+		
+		for (int i = 0; i < absoluto.length-contPontos-html; i++) {
+
+			resposta += absoluto[i]+"/";
+		}
+		
+		for (int i = contPontos; i < relativo.length; i++) {
+			resposta += relativo[i]+"/";
+			
+		}
+		
+		return resposta;
+	}
+	
 	
 	private String removeBarra(String urlRemov) {
 		StringBuffer sb = new StringBuffer();
