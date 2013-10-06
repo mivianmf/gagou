947a30d314cf19a26f232dcfb3f4a86b64c2b131
mivianmarques@gmail.com <mivianmarques@gmail.com@6b8f7fa3-c1a2-4994-f7d0-45643b9cd2d7>
2013-10-06 20:00:52 +0000

    indexer e stemming

---
+++ Gagou/src/indexer/InformacoesTermo.java
@@ -0,0 +1,25 @@
+package indexer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class InformacoesTermo {
+	
+	/** Nome do documento. */
+	String documento;
+	
+	/** Frequência do termo no documento. */
+	int TF;
+	
+	/** Posições do documento que o termo aparece. */
+	List<Integer> posicoes;
+	
+	
+	/** Construtor. */
+	public InformacoesTermo(String documento, int TF) {
+		this.documento = documento;
+		this.TF = TF;
+		posicoes = new LinkedList<Integer>();
+	}
+}
