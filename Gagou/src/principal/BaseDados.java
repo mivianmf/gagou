d51b156fe8804304fa6a9340210eca3511bcebd6
brunosantiagopinheiro <brunosantiagopinheiro@6b8f7fa3-c1a2-4994-f7d0-45643b9cd2d7>
2013-10-17 05:47:09 +0000

    NoSQL adicionado, necessário mongodb instalado

--- Gagou/src/principal/BaseDados.java
+++ Gagou/src/principal/BaseDados.java
@@ -1,54 +1,63 @@
 package principal;
 
-import java.sql.*;
+import java.math.BigInteger;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
 
 public class BaseDados {
-	private String userName;
-	private String userPassword;
 	private String database;
-	private Connection connection;
-	private PreparedStatement query;
-	private ResultSet result;
+	private MongoClient mongoClient;
+	private DB db;
+	private DBCollection coll;
 	
-	public BaseDados(String userName, String userPassword, String database) throws ClassNotFoundException{
-		Class.forName("com.mysql.jdbc.Driver");		
-		this.userName = userName;
-		this.userPassword = userPassword;
+	public BaseDados(String database) throws ClassNotFoundException, UnknownHostException{
 		this.database = database;
+		mongoClient = new MongoClient( "localhost" );
 	}
 	
-	public void conectar( ) throws SQLException{
-		connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/"+database+"?user="+userName+"&password="+userPassword);
-		System.out.println("Conectou!");
-		prepararQueryInsercao( );
+	public void conectar(String collection){
+		db = mongoClient.getDB(database);
+		coll = db.getCollection(collection);
 	}
 	
-	public void desconectar ( ) throws SQLException{
-		connection.close ( );
-		System.out.println("Desconectou!");
+	public void desconectar ( ){
+		this.mongoClient.close();
 	}
 	
-	public void prepararQueryInsercao( ) throws SQLException{
-		query = connection.prepareStatement("INSERT INTO Pagina (PaginaUrlHash, PaginaConteudo, PaginaUrl)"
-		+"values (md5(?), ?, ?");
+	public String md5 (String entrada){
+	    MessageDigest md;
+	    try {
+	    	md = MessageDigest.getInstance("MD5");	    
+	       BigInteger hash = new BigInteger(1, md.digest(entrada.getBytes()));  
+	       String sumario = hash.toString(16);  
+	       return sumario;  
+	    } catch (NoSuchAlgorithmException e) {
+	     // TODO Auto-generated catch block
+	     e.printStackTrace();
+	    }
+		return null;
 	}
 	
-	public void inserir (String paginaUrl, String paginaConteudo) throws SQLException{
-		query.setString(1, paginaUrl);
-		query.setString(2, paginaConteudo);
-		query.setString(3, paginaUrl);
-		query.executeQuery();
+	public void inserir (String paginaUrl, String paginaConteudo){
+		BasicDBObject doc = new BasicDBObject( );
+		doc.put("_id", md5(paginaUrl));
+		doc.put("url", paginaUrl);
+		doc.put("conteudo", paginaConteudo);
+		
+		coll.insert(doc);
 	}
 	
 	/*Função criada apenas para dar um exemplo de como fazer um select. criaremos os selects que forem necessários futuramente.*/
-	public void pesquisar ( ) throws SQLException{
-		Statement statement = connection.createStatement();
-		String query = "SELECT * from Pagina";
-		this.result = statement.executeQuery(query);
-		
-		while (this.result.next()){
-			String string = ""+this.result.getInt("PaginaId")+" "+this.result.getString("PaginaUrlHash")+" "+this.result.getString("PaginaConteudo")+" "+this.result.getString("PaginaUrl");				
-			System.out.println (string);
-		}
+	public void pesquisar (String termo){
+		BasicDBObject query = new BasicDBObject();
+		query.put("name",  java.util.regex.Pattern.compile(" "+termo+" "));
+		DBCursor cursosr = coll.find(query);
 	}
 }
